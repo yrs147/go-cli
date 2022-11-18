28aa8fde7803f8f8d8534db86d760dbb0b8b60d2
yrs147 <yashraj14700728@gmail.com>
2022-11-18 08:40:29 +0530

    cobra integration

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
-package main {
-	import(
-		"fmt"
-		"net/http"
-	)
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/yrs147/go-cli/cmd"
+
+func main() {
+	cmd.Execute()
 }
